16fba9b33bfd5ac5d2b15b2db78b4a0632bc0bc7
bigcream <duyk63uet@gmail.com>
2025-03-15 20:27:44 +0700

    init project

--- src/main/java/com/example/customsonarrule/CustomJavaRulesPlugin.java
+++ src/main/java/com/example/customsonarrule/CustomJavaRulesPlugin.java
@@ -1,8 +1,6 @@
 package com.example.customsonarrule;
 
 import org.sonar.api.Plugin;
-import org.sonar.api.rules.RuleType;
-import org.sonar.api.server.rule.RulesDefinition;
 
 import java.util.List;
 
@@ -18,23 +16,5 @@ public class CustomJavaRulesPlugin implements Plugin {
                 CustomRulesDefinition.class,
                 NoSystemOutPrintRule.class
         );
-    }
-}
-
-class CustomRulesDefinition implements RulesDefinition {
-    private static final String REPOSITORY_KEY = "customjavarules";
-
-    @Override
-    public void define(Context context) {
-        NewRepository repository = context.createRepository(REPOSITORY_KEY, "java")
-                .setName("Custom Java Rules");
-
-        NewRule rule = repository.createRule(NoSystemOutPrintRule.RULE_KEY)
-                .setName("Avoid using System.out.println")
-                .setHtmlDescription("System.out.println should not be used in production code; use a logger instead.")
-                .setType(RuleType.CODE_SMELL)
-                .setSeverity("MAJOR");
-
-        repository.done();
     }
 }
